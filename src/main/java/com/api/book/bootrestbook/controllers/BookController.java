b8e17d51b2dad25b1ac0ed529a02d8e32949afaf
code jstar <codejstar@gmail.com>
2024-10-21 13:23:11 +0530

    create a BookService

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -1,26 +1,35 @@
 package com.api.book.bootrestbook.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
 // import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import com.api.book.bootrestbook.entitiesModel.Book;
+import com.api.book.bootrestbook.services.BookService;
 
 // @Controller
 @RestController
 public class BookController {
 
+    @Autowired
+    private BookService bookService;
+
     // @RequestMapping(value = "/books", method = RequestMethod.GET)
     // @ResponseBody
     @GetMapping("/books")
-    public Book getBook() {
-        Book book = new Book();
-        book.setId(0);
-        book.setAuthor("Jagdeep Jakhu");
-        book.setTitle("Java Complete Guid");
-        return book;
+    public List<Book> getBook() {
+
+        // Book book = new Book();
+        // book.setId(0);
+        // book.setAuthor("Jagdeep Jakhu");
+        // book.setTitle("Java Complete Guid");
+        // return book;
+
+        return this.bookService.getBooks();
     }
 }
---
+++ src/main/java/com/api/book/bootrestbook/services/BookService.java
@@ -0,0 +1,30 @@
+package com.api.book.bootrestbook.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.api.book.bootrestbook.entitiesModel.Book;
+
+public class BookService {
+
+    private static List<Book> list = new ArrayList<>();
+
+    static {
+        list.add(new Book(1, "Java Complete Guid", "xyz"));
+        list.add(new Book(2, "JavaScript Complete Guid", "123"));
+        list.add(new Book(3, "Python Complete Guid", "IIII"));
+    }
+
+    // Get all books
+    public List<Book> getBooks() {
+        return list;
+    }
+
+    // get single book by id
+    public Book getBookById(int id) {
+        Book book = null;
+        book = list.stream().filter(e -> e.getId() == id).findFirst().get();
+        return book;
+    }
+
+}
